cb266a4a1aaf90d42ac87b220bf101d5c32a4ce8
KerstyneW <91274075+KerstyneW@users.noreply.github.com>
2022-06-24 11:04:00 -0400

    Create javascript.js

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+// Javascript used to call linkedin pages reference: https://github.com/KerstyneW/a3web/blob/main/js/javascript.js
+function LinkedinKW(){
+    window.open('https://www.linkedin.com/in/kerstyne%2Dwidmeyer%2D733010200');
+}
+function LinkedinJL(){
+    window.open('https://www.linkedin.com/in/jade%2Dlacsamana%2Da7224b131/')
+}
+function LinkinJK(){
+    window.open('https://www.linkedin.com/in/james-kwasnicki-481a20221/')
+}
+function Webapp() {
+    window.open('https://experience.arcgis.com/experience/2176933e8c1d4b3aaf37df590692b5f4/')
+}
